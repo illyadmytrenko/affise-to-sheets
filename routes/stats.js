e7d0495378ca9040f3ebd6eb420944195653bb87
illyadmytrenko <98984669+illyadmytrenko@users.noreply.github.com>
2025-09-24 10:29:29 +0200

    added limiter

--- routes/stats.js
+++ routes/stats.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import stats from "../controllers/stats.js";
+import rateLimit from "express-rate-limit";
 
 const router = Router();
 
-router.get("/conversions", stats.getConversions);
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 3,
+  message: { error: "Занадто багато запитів. Спробуйте через хвилину." },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+router.get("/conversions", stats.getConversions, limiter);
 
 export default router;
